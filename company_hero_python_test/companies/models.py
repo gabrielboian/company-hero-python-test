6da11b9f978536b0e5446eaa40bf677770b7155e
Gabriel Boian <gabrielboiancm@gmail.com>
2021-06-10 22:41:32 -0300

    make inital commits and change db backends to use postgres

--- company_hero_python_test/companies/models.py
+++ company_hero_python_test/companies/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+class Company(models.Model):
+    cnpj = models.CharField(max_length=15, null=True, blank=True)
+    name = models.CharField(max_length=255, null=False, blank=False)
+    users = models.ManyToManyField('users.User', _('users'), blank=False)
+    
+    class Meta:
+        db_table = 'company'
+        verbose_name = _('company')
+        verbose_name_plural = _('companies')
+    
+    
+    def get_all_users(self):
+        print(self.users.name)
+        return self.users.all()
