20afdf13e4c9edbf67e20228b70965f89eaf2b2f
Gabriel Boian <gabrielboiancm@gmail.com>
2021-06-10 21:50:30 -0300

    create file urls and managers and create models to users handle

--- company_hero_python_test/users/models.py
+++ company_hero_python_test/users/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
+from django.core.mail import send_mail
 
-# Create your models here.
+from .managers import CustomUserManager
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = None
+    first_name = models.CharField(_('first name'), max_length=50, blank=True, null=True)
+    last_name = models.CharField(_('last name'), max_length=50, blank=True, null=True)
+    email = models.EmailField(_('email address'), max_length=100, unique=True)
+    is_staff = models.BooleanField(_('staff status'), default=False)
+    is_active = models.BooleanField(_('active'), default=True)
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+    companies = models.ManyToManyField('companies.Company', _('companies'), blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name']
+
+    objects = CustomUserManager()
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def get_full_name(self):
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        return self.first_name
+    
+    def email_user(self, subject, message, from_email=None):
+        send_mail(subject, message, 'email@companyhero.com', [self.email])
